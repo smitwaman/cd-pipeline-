cdba24c6dddf1d5a04cf188c5bf821329b3e0adf
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-21 12:58:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/anthonyvetter/argocd-getting-started.git && cd argocd-getting-started
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sudo watch kubectl get pods -n argocd
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
-sudo kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+watch kubectl get pods -n argocd
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
