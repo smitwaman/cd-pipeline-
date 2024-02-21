9a9efd5215792c12f7b409a85bcf5ecd0bee3266
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-21 13:05:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 watch kubectl get pods -n argocd
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+
+ kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
